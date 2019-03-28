5b989dd001d23179996c075e728751bc294b6707
nicoleyimessier <nicolemessier@localprojects.com>
2019-03-28 16:00:16 -0400

    updated toggle editing to void

--- src/ofxWarp/Controller.cpp
+++ src/ofxWarp/Controller.cpp
@@ -243,7 +243,7 @@ namespace ofxWarp
     }
     
     //--------------------------------------------------------------
-    bool Controller::toggleEditing()
+    void Controller::toggleEditing()
     {
         editingMode = !editingMode;
         
